8f194fccbca2da8f33d96c6c498650c92ebc71f0
Joshua B <desertflame3@gmail.com>
2024-05-17 13:36:09 -0400

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -384,23 +384,6 @@ export const ChatInput = ({
           )}
         </div>
       </div>
-      <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-       <p className='text-black/80 dark:text-white/80'>
-          <strong>ATTN:</strong> gpt.shopify.io is being deprecated in favor of: <a href="https://chat.shopify.io"
-          target="_blank"
-          className="underline font-bold tracking-wider dark:decoration-black/50 dark:decoration-white/50">
-          chat.shopify.io
-          </a>.
-        </p>
-        <p className='text-black/80 dark:text-white/80'>
-        Please use the new chat interface for future interactions.
-        </p>
-        <p>
-        {t(
-          "Shopify-internal information ok, no PII.",
-        )}
-        </p>
-      </div>
     </div>
   );
 };
